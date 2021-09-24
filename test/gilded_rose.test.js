87d3388d8a205078b7691dd92b0196bfd5ad1efd
Pedro Henrique <phcatanduba@gmail.com>
2021-09-24 20:02:32 -0300

    test: legendary + random item tests

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -2,9 +2,19 @@ import Shop from '../src/gilded_rose.js';
 import Item from '../src/items/item.js';
 
 describe('Gilded Rose', function () {
-    it('should foo', function () {
-        const gildedRose = new Shop([new Item('foo', 0, 0)]);
+    test('should sellIn and quality update as a random item', () => {
+        const gildedRose = new Shop([new Item('foo', 1, 1)]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).toBe('foo');
+        expect(items[0].sellIn).toBe(0);
+        expect(items[0].quality).toBe(0);
+    });
+
+    test('legendary item do nothing on update', () => {
+        const gildedRose = new Shop([
+            new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+        ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toBe(0);
+        expect(items[0].quality).toBe(80);
     });
 });
