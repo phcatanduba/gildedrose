ad6a9b1a0cec29a336463addec86b25e6f5e5368
Pedro Henrique <phcatanduba@gmail.com>
2021-09-24 20:33:49 -0300

    fix: fixing backstage update

--- src/items/backstage-pass.js
+++ src/items/backstage-pass.js
@@ -5,15 +5,14 @@ export default class BackstagePass extends Item {
         super('Backstage passes to a TAFKAL80ETC concert', sellIn, quality);
     }
 
-    rawQuality() {
-        if (this.sellIn < 0) return 0;
-        if (this.sellIn <= 5) return this.quality + 3;
-        if (this.sellIn <= 10) return this.quality + 2;
-        return this.quality + 1;
-    }
-
     update() {
-        this.quality = Math.min(50, this.rawQuality());
         this.sellIn -= 1;
+        let newQuality;
+        if (this.sellIn < 0) newQuality = 0;
+        else if (this.sellIn <= 4) newQuality = this.quality + 3;
+        else if (this.sellIn <= 9) newQuality = this.quality + 2;
+        else newQuality = this.quality + 1;
+
+        this.quality = Math.min(50, newQuality);
     }
 }
